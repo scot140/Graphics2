87858027c8a2f3f49e608146e96bc8ac8ad4d879
David Ashbourne <scot140@gmail.com>
2015-07-06 18:13:07 -0400

    First inital push

---
+++ Graphic2project/source/VertexShader.hlsl
@@ -0,0 +1,48 @@
+#pragma pack_matrix(row_major)
+
+struct INPUT_VERTEX
+{
+	float4 pos : POSITION;
+	float4 col : COLOR;
+	float2 uv : UV;
+};
+
+struct OUTPUT_VERTEX
+{
+	float4 projectedCoordinate : SV_POSITION;
+	float2 uvOut : UV;
+	float4 colorOut : COLOR;
+};
+
+cbuffer OBJECT : register(b0)
+{
+	float4x4 WorldMatrix;
+};
+
+// TODO: PART 3 STEP 2a
+cbuffer SCENE : register(b1)
+{
+	float4x4 viewMatrix;
+	float4x4 projectionMatrix;
+};
+
+OUTPUT_VERTEX main(INPUT_VERTEX fromVertexBuffer)
+{
+	OUTPUT_VERTEX sendToRasterizer = (OUTPUT_VERTEX)0;
+
+	float4 output = fromVertexBuffer.pos;
+
+	output = mul(output, WorldMatrix);
+
+	output = mul(output, viewMatrix);
+
+	output = mul(output, projectionMatrix);
+
+	sendToRasterizer.projectedCoordinate = output;
+
+	sendToRasterizer.colorOut = fromVertexBuffer.col;
+
+	sendToRasterizer.uvOut = fromVertexBuffer.uv;
+
+	return sendToRasterizer;
+}
